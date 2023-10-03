d2ad00f479e27821fd27bcb6b21a8c9a15d3e4b3
Simbhakk <127213964+Simbhakk@users.noreply.github.com>
2023-10-03 16:22:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+import os
 class Config:
-    API_ID = ""
-    API_HASH = ""
-    TOKEN = ""  
-    START_PIC = "" 
-    CHAT = ""    
+    API_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    TOKEN = os.environ.get("TOKEN")
+    START_PIC = os.environ.get("START_PIC")
+    CHAT = os.environ.get("CHAT")
